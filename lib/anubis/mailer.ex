61600c13471da6f0d7108a04f9484354329040ed
Syed Ibna Zubayear <47857423+Zubayear@users.noreply.github.com>
2025-04-12 23:46:41 +0600

    first commit

---
+++ lib/anubis/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Anubis.Mailer do
+  use Swoosh.Mailer, otp_app: :anubis
+end
---
+++ lib/anubis_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule AnubisWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use AnubisWeb, :html
+
+  embed_templates "page_html/*"
+end
